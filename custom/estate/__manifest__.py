bcca2e6f1611e28b27b1295a3f13c4ad4577c569
Divyanshu <dgdivyanshu+github@gmail.com>
2021-06-16 20:59:26 +0000

    [FEAT] Added Action, Menu Item, Access Rights,Manifest updated

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -3,7 +3,12 @@
     'version': '1.0',
     'category': 'Sales',
     'depends': ['base','web'],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
     'installable': True,
     'application': True,
-    'auto_install': False
+    'auto_install': True
 }
