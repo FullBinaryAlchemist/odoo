5eb4a47d83766fafd5fed05df1bfe75f979c3f5c
Divyanshu <dgdivyanshu+github@gmail.com>
2021-06-16 09:52:09 +0000

    feat: Added functional models for real estate

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real Estate Application',
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': ['base','web'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
